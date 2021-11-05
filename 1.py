a12b7a3d390a40398260196fe7d619334a001b8a
민혁GIT <seomh81@naver.com>
2021-11-05 22:42:54 +0900

    기울기평가 추가

--- 1.py
+++ 1.py
@@ -52,7 +52,7 @@ def start_buytrade(buy_amt, except_items):
                 vol_before4 = df_candle[4]['candle_acc_trade_volume']
                 vol_before5 = df_candle[5]['candle_acc_trade_volume']
 
-                can_tradeNow = df_candle[0]['trade_price']
+                #can_tradeNow = df_candle[0]['trade_price']
                 can_highNow = df_candle[0]['high_price']
                 can_highBefore1 = df_candle[1]['high_price']
                 can_highBefore2 = df_candle[2]['high_price']
@@ -66,14 +66,14 @@ def start_buytrade(buy_amt, except_items):
                 can_lowBefore4 = df_candle[4]['low_price']
                 can_lowBefore5 = df_candle[5]['low_price']
 
-                can_gapNow = can_highNow - can_lowNow
+                #can_gapNow = can_highNow - can_lowNow
                 can_gapBefore1 = can_highBefore1 - can_lowBefore1
                 can_gapBefore2 = can_highBefore2 - can_lowBefore2
                 can_gapBefore3 = can_highBefore3 - can_lowBefore3
                 can_gapBefore4 = can_highBefore4 - can_lowBefore4
                 can_gapBefore5 = can_highBefore5 - can_lowBefore5
 
-                can_eval = can_gapNow - (can_gapBefore1 + can_gapBefore2)# + can_gapBefore3 + can_gapBefore4 + can_gapBefore5) * 1
+                #can_eval = can_gapNow - (can_gapBefore1 + can_gapBefore2)# + can_gapBefore3 + can_gapBefore4 + can_gapBefore5) * 1
                 vol_eval = vol_tradeNow - (vol_before1 + vol_before2 + vol_before3 + vol_before4 + vol_before5) * 0.5
 
                 # 볼린저밴드 15분봉
@@ -85,18 +85,33 @@ def start_buytrade(buy_amt, except_items):
                 bb_Before3 = df_bb[3]['BBL']
                 bb_Before4 = df_bb[4]['BBL']
                 bb_Before5 = df_bb[5]['BBL']
+                bb_Before6 = df_bb[6]['BBL']
+                bb_Before7 = df_bb[7]['BBL']
+                bb_Before8 = df_bb[8]['BBL']
+                bb_Before9 = df_bb[9]['BBL']
                 bb_Before10 = df_bb[10]['BBL']
                 bb_gapBefore1 = bb_Before1 - bb_Before2
-                bb_gapBefore2 = bb_Before2 - bb_Before3
-                bb_gapBefore3 = bb_Before3 - bb_Before4
-                bb_gapBefore4 = bb_Before4 - bb_Before5
-                bb_gapBefore2to10 = bb_Before2 - bb_Before10
-                bb_eval = bb_gapBefore1 - (bb_gapBefore2to10 / 8)
+                bb_gapBefore2 = (bb_Before1 - bb_Before3) / 2
+                bb_gapBefore3 = (bb_Before1 - bb_Before4) / 3
+                bb_gapBefore4 = (bb_Before1 - bb_Before5) / 4
+                bb_gapBefore5 = (bb_Before1 - bb_Before6) / 5
+                bb_gapBefore6 = (bb_Before1 - bb_Before7) / 6
+                bb_gapBefore7 = (bb_Before1 - bb_Before8) / 7
+                bb_gapBefore8 = (bb_Before1 - bb_Before9) / 8
+                bb_gapBefore9 = (bb_Before1 - bb_Before10) / 9
+                bb_eval1 = bb_gapBefore1 - bb_gapBefore2
+                bb_eval2 = bb_gapBefore1 - bb_gapBefore3
+                bb_eval3 = bb_gapBefore1 - bb_gapBefore4
+                bb_eval4 = bb_gapBefore1 - bb_gapBefore5
+                bb_eval5 = bb_gapBefore1 - bb_gapBefore6
+                bb_eval6 = bb_gapBefore1 - bb_gapBefore7
+                bb_eval7 = bb_gapBefore1 - bb_gapBefore8
+                bb_eval8 = bb_gapBefore1 - bb_gapBefore9
 
-                print("BBL", format((bb_now - can_lowNow) / bb_now * 100, '.2f'),"%",item_list_for['market'], "  BB추세", int(bb_eval) , "  거래량",format(vol_eval * 100 / (vol_tradeNow + vol_before1 + vol_before2 + vol_before3 + vol_before4 + vol_before5),'.0f'), "%---양수TRY / 제외종목:", except_items)
+                print("BBL", format((bb_now - can_lowNow) / bb_now * 100, '.2f'),"%",item_list_for['market'], "  BB추세", int(bb_eval1) , "  거래량",format(vol_eval * 100 / (vol_tradeNow + vol_before1 + vol_before2 + vol_before3 + vol_before4 + vol_before5),'.0f'), "%---양수TRY / 제외종목:", except_items)
 
                 # 볼린저밴드 15분봉 하단을 찍을 때 매수
-                if bb_now > can_lowNow and bb_eval > 0 and vol_eval > 0 and can_lowNow < can_lowBefore1 and can_lowNow < can_lowBefore2 and can_lowNow < can_lowBefore3 and can_gapBefore1 != 0 and can_gapBefore2 != 0 and can_gapBefore3 != 0 and can_gapBefore4 != 0 and can_gapBefore5 != 0:
+                if bb_now > can_lowNow and bb_eval1 > 0 and bb_eval2 > 0 and bb_eval3 > 0 and bb_eval4 > 0 and bb_eval5 > 0 and bb_eval6 > 0 and bb_eval7 > 0 and bb_eval8 > 0 and vol_eval > 0 and can_lowNow < can_lowBefore1 and can_lowNow < can_lowBefore2 and can_lowNow < can_lowBefore3 and can_gapBefore1 != 0 and can_gapBefore2 != 0 and can_gapBefore3 != 0 and can_gapBefore4 != 0 and can_gapBefore5 != 0 and can_highBefore1 != can_highBefore2 and can_highBefore1 != can_highBefore3 and can_highBefore1 != can_highBefore4 and can_highBefore1 != can_highBefore5:
 
                     # 기준 충족 종목 종가
                     print(item_list_for['market'],'하한가' + str(can_lowNow))
