2581f1134db17390a00c3ae91ff545b8558c5400
10351335 <minhyuk.seo@tkeap.com>
2021-11-15 15:07:30 +0900

    office

--- 2.6.py
+++ 2.6.py
@@ -46,7 +46,7 @@ def start_selltrade(sell_pcnt, dcnt_pcnt):
             # -----------------------------------------------------------------
             for target_item in target_items:
                 for ticker in tickers:
-                    time.sleep(0.05)
+                    time.sleep(0.07)
                     if target_item['market'] == ticker['market']:
 
                         # -----------------------------------------------------
@@ -162,7 +162,7 @@ if __name__ == '__main__':
 
         # 1. 로그레벨
         log_level = 'I'#input("로그레벨(D:DEBUG, E:ERROR, 그 외:INFO) : ").upper()
-        sell_pcnt = 1.5#input("매도 수익률(ex:2%=2) : ")
+        sell_pcnt = 1.2#input("매도 수익률(ex:2%=2) : ")
         dcnt_pcnt = -1#input("고점대비 하락률(ex:-1%=-1) : ")
 
         upbit.set_loglevel(log_level)
