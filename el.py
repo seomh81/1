9eff5eaa320334e61e7cde36fcd203c88dbac325
10351335 <minhyuk.seo@tkeap.com>
2021-12-03 18:38:31 +0900

    office

--- el.py
+++ el.py
@@ -3,6 +3,10 @@ import time
 from bs4 import BeautifulSoup
 from lib import upbit_new as upbit
 
+t4 = ''
+t44 = ''
+t444 = ''
+
 while True:
 
     search_keyword = '엘리베이터'
@@ -22,13 +26,15 @@ while True:
         #라인에 보내기 내용보내기
         #upbit.send_line_message(title['title'])
     print(t1)
+    t3 = t1
 
-    if t1 != t1:
-        upbit.send_line_message(title['title'])
+    if t3 != t4:
+        print('내용이 변경되어 라인으로 전송합니다!!')
+        upbit.send_line_message(t3)
 
     time.sleep(3)
 
-
+    t4 = t3
 
     search_keyword = '승강기'
     url = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query={search_keyword}'
@@ -39,22 +45,26 @@ while True:
 
     print('총', len(news_titles), '개의 뉴스 제목이 있습니다')
     print()
-    t1 = ''
+    t11 = ''
     for title in news_titles:
-        #print(title['title'])
-        t2 = title['title']
-        t1 = t1 + '\n' + t2
-        #라인에 보내기 내용보내기
-        #upbit.send_line_message(title['title'])
-    print(t1)
+        # print(title['title'])
+        t22 = title['title']
+        t11 = t11 + '\n' + t22
+        # 라인에 보내기 내용보내기
+        # upbit.send_line_message(title['title'])
+    print(t11)
+    t33 = t11
 
-    if t1 != t1:
-        upbit.send_line_message(title['title'])
+    if t33 != t44:
+        print('내용이 변경되어 라인으로 전송합니다!!')
+        upbit.send_line_message(t33)
 
     time.sleep(3)
 
+    t44 = t33
 
-    search_keyword = '티케이엘리베이터'
+
+    search_keyword = '속보'
     url = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query={search_keyword}'
 
     r = requests.get(url)
@@ -63,16 +73,20 @@ while True:
 
     print('총', len(news_titles), '개의 뉴스 제목이 있습니다')
     print()
-    t1 = ''
+    t111 = ''
     for title in news_titles:
-        #print(title['title'])
-        t2 = title['title']
-        t1 = t1 + '\n' + t2
-        #라인에 보내기 내용보내기
-        #upbit.send_line_message(title['title'])
-    print(t1)
+        # print(title['title'])
+        t222 = title['title']
+        t111 = t111 + '\n' + t222
+        # 라인에 보내기 내용보내기
+        # upbit.send_line_message(title['title'])
+    print(t111)
+    t333 = t111
 
-    if t1 != t1:
-        upbit.send_line_message(title['title'])
+    if t333 != t444:
+        print('내용이 변경되어 라인으로 전송합니다!!')
+        upbit.send_line_message(t333)
 
     time.sleep(3)
+
+    t444 = t333
