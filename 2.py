bfc15eb8847a5b57d102d7af16740cbc5d7f6c58
smh <seomh81@naver.com>
2022-03-25 22:21:05 +0900

    new 220325

--- 2.py
+++ 2.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
         # 1. 로그레벨
         log_level = 'I' #input("로그레벨(D:DEBUG, E:ERROR, 그 외:INFO) : ").upper()
         sell_pcnt = 4 #input("매도 수익률(ex:2%=2) : ")
-        dcnt_pcnt = -2 #input("고점대비 하락률(ex:-1%=-1) : ")
+        dcnt_pcnt = -3 #input("고점대비 하락률(ex:-1%=-1) : ")
 
         upbit.set_loglevel(log_level)
 
