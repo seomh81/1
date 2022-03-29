19fffbc8bcf484fe19f91b533ea0a464ea84aeb5
smh <seomh81@naver.com>
2022-03-29 23:16:05 +0900

    new 220329

--- cron/1monitoring.sh
+++ cron/1monitoring.sh
@@ -2,7 +2,8 @@
 export PATH=$PATH:/usr/local/bin
 
 # Change Directory
-cd ~/1/
+#cd ~/1/
+cd
 
 # Run Program
 python3 ./1/1.py I >> ~/1/logs/1monitoring.log 2>&1
--- cron/2monitoring.sh
+++ cron/2monitoring.sh
@@ -2,7 +2,8 @@
 export PATH=$PATH:/usr/local/bin
 
 # Change Directory
-cd ~/1/
+#cd ~/1/
+cd
 
 # Run Program
 python3 ./1/2.py I >> ~/1/logs/2monitoring.log 2>&1
