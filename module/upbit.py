90a85e3798fe550d2d8bb3c4e377e77c1efe7be4
민혁GIT <seomh81@naver.com>
2022-08-11 00:55:31 +0900

    home

--- module/upbit.py
+++ module/upbit.py
@@ -40,7 +40,7 @@ line_target_url = 'https://notify-api.line.me/api/notify'
 # -----------------------------------------------------------------------------
 def get_env_keyvalue(key):
     try:
-        path = './env/env.txt'
+        path = 'env4linux.txt'
 
         f = open(path, 'r', encoding='UTF8')
         line = f.readline()
