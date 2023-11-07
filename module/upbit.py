606b1a3c9916ab515756beb79c852a06db9f3e7a
seomh81 <seomh81@gmail.com>
2023-11-08 00:45:28 +0900

    macbookAir M2

--- module/upbit.py
+++ module/upbit.py
@@ -1194,8 +1194,8 @@ def get_bb(candle_datas):
             # 표준편차(곱)
             unit = 2
 
-            band1 = unit * numpy.std(df[len(df) - 50:len(df)]) #와.,.. 볼밴이 두개였다니... 그래서 안고쳐지고 오작동한거임???
-            bb_center = numpy.mean(df[len(df) - 50:len(df)])
+            band1 = unit * numpy.std(df[len(df) - 100:len(df)]) #와.,.. 볼밴이 두개였다니... 그래서 안고쳐지고 오작동한거임???
+            bb_center = numpy.mean(df[len(df) - 100:len(df)])
             band_high = bb_center + band1
             band_low = bb_center - band1
 
