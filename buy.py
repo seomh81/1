28eb0fc04548929ecf2994b87991a079b3cc6438
seomh81 <seomh81@gmail.com>
2024-01-09 11:00:09 +0900

    macbookAir M2

--- buy.py
+++ buy.py
@@ -52,21 +52,8 @@ def start_buytrade(buy_amtp):
                 logging.info('체크중....[' + str(target_item['market']) + ']')
                 #time.sleep(0.001)
 
-                # -------------------------------------------------------------
-                # 종목별 보조지표를 조회
-                # 1. 조회 기준 : 일캔들, 최근 5개 지표 조회
-                # 2. 속도를 위해 원하는 지표만 조회(RSI, MFI, MACD, CANDLE) - 수정
-                # -------------------------------------------------------------
-                indicators = upbit.get_indicator_sel(target_item['market'], '30', 200, 51,
-                                                     ['BB', 'BB2', 'CANDLE'])
-                                                            #['RSI', 'MFI', 'MACD', 'BB', 'CANDLE'])
 
-                # --------------------------------------------------------------
-                # 최근 상장하여 캔들 갯수 부족으로 보조 지표를 구하기 어려운 건은 제외
-                # --------------------------------------------------------------
-                if 'CANDLE' not in indicators or len(indicators['CANDLE']) < 200: #원래 200
-                    logging.info('캔들 데이터 부족으로 데이터 산출 불가...[' + str(target_item['market']) + ']')
-                    continue
+
 
                 # ------------------------------------------------------------------
                 # 잦은 거래 방지
@@ -92,16 +79,34 @@ def start_buytrade(buy_amtp):
                 # 시간 차이 추출
                 diff = current_dt - last_buy_dt
 
-                logging.info(str(last_buy_dt) + '     ' + str(current_dt))
-                logging.info(str(diff) + '시간 차이')
-                logging.info(str(diff.seconds) + '시간 차이')
+                # logging.info(str(last_buy_dt) + '     ' + str(current_dt))
+                # logging.info(str(diff) + '시간 차이')
+                # logging.info(str(diff.seconds) + '초 차이') # 초 계산이 안맞네;;;
+                #
+                # logging.info(str(diff.days) + '날 차이')
 
                 # 매수 후 10시간은 진행하지 않음(너무 잦은 거래 방지) - 잠시 꺼두자
-                if diff.seconds < 36000:
-                    logging.info('+_+ 매수 직후 발생하는 빈번하게 진행하지 않음!!! 10시간 대기조 +_+ ')
+                if diff.days < 1:
+                    logging.info('+_+ 하루에 한 번 거래 +_+ ')
                     logging.info('------------------------------------------------------')
                     continue
 
+                # -------------------------------------------------------------
+                # 종목별 보조지표를 조회
+                # 1. 조회 기준 : 일캔들, 최근 5개 지표 조회
+                # 2. 속도를 위해 원하는 지표만 조회(RSI, MFI, MACD, CANDLE) - 수정
+                # -------------------------------------------------------------
+                indicators = upbit.get_indicator_sel(target_item['market'], '30', 200, 51,
+                                                     ['BB', 'BB2', 'CANDLE'])
+                # ['RSI', 'MFI', 'MACD', 'BB', 'CANDLE'])
+
+
+                # --------------------------------------------------------------
+                # 최근 상장하여 캔들 갯수 부족으로 보조 지표를 구하기 어려운 건은 제외
+                # --------------------------------------------------------------
+                if 'CANDLE' not in indicators or len(indicators['CANDLE']) < 200: #원래 200
+                    logging.info('캔들 데이터 부족으로 데이터 산출 불가...[' + str(target_item['market']) + ']')
+                    continue
 
                 # --------------------------------------------------------------
                 # 보조 지표 추출
@@ -133,13 +138,14 @@ def start_buytrade(buy_amtp):
 
                 logging.info("{:,}".format(krw_balance['krw_balance']) + ' 원 매수 가능 --> ' + str(avg_buy_price) + ' 원 기매수 금액 --> ' \
                              + str(buy_amt) + ' 원 매수 시도 (' + str(buy_amtp) + ' %)')
-                logging.info(str(round((candle[0]['trade_price'] - bb2[0]['BBL']) / bb2[0]['BBL'] * 100, 1)) + ' %')
+                logging.info(str(round((candle[0]['trade_price'] - bb2[0]['BBL']) / bb2[0]['BBL'] * 100, 1)) + ' % BBL diff. || BBH diff. ' + str(round((bb2[0]['BBH'] - candle[0]['trade_price']) / bb2[0]['BBH'] * 100, 1)) + ' %')
 
                 # if krw_balance['krw_balance'] + avg_buy_price > 1400000:
                 #     buy_amt = buy_amt + 30000
                 # else:
                 #     buy_amt = buy_amt - 30000
 
+                i = 0 # i 초기화
                 # --------------------------------------------------------------
                 # 볼린저 밴드 추가
                 # --------------------------------------------------------------
@@ -302,7 +308,7 @@ if __name__ == '__main__':
         # 1. 로그레벨
         log_level = 'I'#input("로그레벨(D:DEBUG, E:ERROR, 그 외:INFO) : ").upper()
         # buy_amt = 20000#input("매수금액(M:최대, 10000:1만원) : ").upper()
-        buy_amtp = 10 #몇 %씩 매수할지?
+        buy_amtp = 1 #몇 %씩 매수할지?
         upbit.set_loglevel(log_level)
 
         logging.info("*********************************************************")
