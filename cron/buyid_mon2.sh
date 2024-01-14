28abf01e25974d800122f3f879a3ec65a1968b92
seomh81 <seomh81@gmail.com>
2024-01-14 21:22:40 +0900

    macbookAir M2

--- cron/buyid_mon2.sh
+++ cron/buyid_mon2.sh
@@ -9,7 +9,7 @@ if [ -z "$PID" ];
 then
         echo "$timestamp"
         echo 'MONITORING PROCESS DEAD'
-        ~/1/cron/buy_mon.sh
+        ~/1/cron/buy_mon2.sh
 else
         echo "$timestamp"
         echo 'MONITORING PROCESS IS RUNNING'
